d6b79d905e9b3c19373e5cf3c6ed1af63f299e19
Andy Walter <musictopia2@hotmail.com>
2025-12-03 18:23:27 -0700

    Converted to using the new extension model in c#.

--- UploadHelpers/LocalFileSaver.cs
+++ UploadHelpers/LocalFileSaver.cs
@@ -4,7 +4,7 @@ public class LocalFileSaver : IFileSaver
     private readonly IConfiguration _configuration = bb1.Configuration ?? throw new CustomBasicException("Needs IConfiguration Registered");
     async Task<string> IFileSaver.SaveAsync(FileSaveContext context)
     {
-        string basePath = _configuration.GetUploadSavePath();
+        string basePath = _configuration.GetUploadSavePath;
         // Sanitize filename or generate fallback
         string safeFileName = Path.GetFileName(context.OriginalFileName ?? Guid.NewGuid().ToString());
 
